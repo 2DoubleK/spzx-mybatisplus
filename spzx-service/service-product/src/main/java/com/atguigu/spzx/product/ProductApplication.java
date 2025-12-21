db8cc2598c283f86a223a941277187d79f9af006
root <17383958898@163.com>
2025-12-21 21:02:08 +0800

    springCache+redis使用改善所有分类查询

--- spzx-service/service-product/src/main/java/com/atguigu/spzx/product/ProductApplication.java
+++ spzx-service/service-product/src/main/java/com/atguigu/spzx/product/ProductApplication.java
@@ -3,9 +3,11 @@ package com.atguigu.spzx.product;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @MapperScan(basePackages = {"com.atguigu.spzx.product.mapper"})
+@EnableCaching
 public class ProductApplication {
 
     public static void main(String[] args) {
--- spzx-service/service-product/src/main/java/com/atguigu/spzx/product/service/impl/CategoryServiceImpl.java
+++ spzx-service/service-product/src/main/java/com/atguigu/spzx/product/service/impl/CategoryServiceImpl.java
@@ -1,30 +1,59 @@
 package com.atguigu.spzx.product.service.impl;
 
+import com.alibaba.cloud.commons.lang.StringUtils;
+import com.alibaba.fastjson.JSON;
 import com.alibaba.nacos.client.naming.utils.CollectionUtils;
 import com.atguigu.spzx.model.entity.product.Category;
 import com.atguigu.spzx.product.mapper.CategoryMapper;
 import com.atguigu.spzx.product.service.CategoryService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
+
+import static com.atguigu.spzx.product.utils.Constants.CAT_ONE_KEY;
+import static com.atguigu.spzx.product.utils.Constants.EXPIRE_TIME;
+
 
 @Service
 public class CategoryServiceImpl extends ServiceImpl<CategoryMapper, Category> implements CategoryService {
     @Autowired
     private CategoryMapper categoryMapper;
 
+    @Autowired
+    private RedisTemplate<String, String> redisTemplate;
+
     @Override
     public List<Category> findOneCategory() {
-        return categoryMapper.findAllTopCate();
+        //先从redis中查询一级分类
+        String cat_one_json = redisTemplate.opsForValue().get(CAT_ONE_KEY);
+        //有，返回缓存
+        if (StringUtils.isNotBlank(cat_one_json)) {
+            return JSON.parseArray(cat_one_json, Category.class);
+        }
+        //没有在查数据库，将查到的结果放到redis
+        List<Category> categories = categoryMapper.findAllTopCate();
+        try {
+            if (categories != null) {
+                //放入redis
+                redisTemplate.opsForValue().set(CAT_ONE_KEY, JSON.toJSONString(categories), EXPIRE_TIME, TimeUnit.MINUTES);
+            }
+        } catch (Exception e) {
+            log.error("写入redis失败{}",e);
+        }
+        return categories;
     }
 
     //查询所有分类按照树形结构封装
+    @Cacheable(value = "category",key = "'all'")   //放入redis的结果就是category::all
     @Override
     public List<Category> findCategoryTree() {
         //1.查询所有分类
