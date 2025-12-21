41a4b979e27c9180b65f1d5c9056534467419160
root <17383958898@163.com>
2025-12-21 13:34:58 +0800

    日志接口

--- spzx-manager/src/main/java/com/atguigu/spzx/manager/ManagerApplication.java
+++ spzx-manager/src/main/java/com/atguigu/spzx/manager/ManagerApplication.java
@@ -11,6 +11,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @ComponentScan(basePackages = {"com.atguigu.spzx"})  //Knife4jConfig即，swagger不和当前服务在一个包下，额外配置
 @EnableConfigurationProperties(value = {UserProperties.class}) //开启配置文件读取
 @EnableScheduling
+@org.mybatis.spring.annotation.MapperScan(basePackages = {"com.atguigu.spzx.manager.mapper", "com.atguigu.spzx.common.log.mapper"})
 public class ManagerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ManagerApplication.class,args);
